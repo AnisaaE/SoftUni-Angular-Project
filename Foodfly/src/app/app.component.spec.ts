bee414acb795fd3cd5dba46a108f486086663eeb
Anisa <anisaemin97@gmail.com>
2023-07-30 02:41:07 +0300

    create login

--- Foodfly/src/app/app.component.spec.ts
+++ Foodfly/src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Foodfly'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Foodfly');
-  });
+  // it(`should have as title 'Foodfly'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('Foodfly');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
---
+++ Foodfly/src/app/core/core.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    NavigationComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports:[FooterComponent, NavigationComponent]
+})
+export class CoreModule { }
